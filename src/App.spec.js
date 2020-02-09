b5b4d6456707bb750aeb3bfcf39c91d531e4d289
amalboukili <boukili.amal@gmail.com>
2020-02-09 20:38:33 +0100

    tester avec chai chai-dirty et chai-chai-jest-diff

--- src/App.spec.js
+++ src/App.spec.js
@@ -7,3 +7,7 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+// it('demos', () => {
+//   expect({ name: 'Joe' }).toEqual({ name: 'Jane' });
+// });
