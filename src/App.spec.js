adbb1b2d5ae78866ac4550eeea3b1a9dcd12360d
amalboukili <boukili.amal@gmail.com>
2020-02-11 10:44:27 +0100

    test with enzyme

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+// import ReactDOM from 'react-dom';
 
 import App from './App';
+import GuessCount from './components/GuessCount/GuessCount';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('<App/>', () => {
+  it('contains a zero-guess counter', () => {
+    // const div = document.createElement('div');
+    // ReactDOM.render(<App />, div);
+    const wrapper = shallow(<App />);
+    expect(wrapper).to.contain(<GuessCount guesses={0} />);
+  });
+
+  it('has 36 cards', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Card')).to.have.length(36);
+  });
 });
-
-// it('demos', () => {
-//   expect({ name: 'Joe' }).toEqual({ name: 'Jane' });
-// });
